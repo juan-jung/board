e80d73ef16717050362574e6455898a467e71809
juan <crumbled23@gmail.com>
2025-01-30 15:04:05 +0900

    #18-게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다
    구현 중인 테스트이기 때문에 실패하면 `gradle build`가 실패하여 배포 자동화에 악영향을 미치는 경우가 있으므로, 실패 테스트는 제외 처리하는 방향으로 정책 설정
    이에 기능 구현 중인 유닛 테스트를 모두 `@Disabled` 처리

---
+++ project-board/src/main/java/com/fcboard/projectboard/controller/ArticleController.java
@@ -0,0 +1,15 @@
+package com.fcboard.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /articles
+ * /articles/{article-id}
+ * /article/search
+ * /article/search-hashtag
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
