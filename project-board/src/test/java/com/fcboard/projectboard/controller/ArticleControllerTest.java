3080b97cde9d042a486247f952e6db067bbd66f4
juan <crumbled23@gmail.com>
2025-02-11 19:13:46 +0900

    #18-게시글 뷰 기본 골격 작성
    컨트롤러, 뷰 테스트까지 한번에 적용

--- project-board/src/test/java/com/fcboard/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fcboard/projectboard/controller/ArticleControllerTest.java
@@ -20,7 +20,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -34,7 +33,6 @@ class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
